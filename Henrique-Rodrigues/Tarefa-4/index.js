50eff1b5bc10da4438809a89ccc04972274f481f
unknown <hrodriguesrabellovieira@gmail.com>
2026-01-30 10:24:27 -0300

    Realização da tarefa 4

---
+++ Henrique-Rodrigues/Tarefa-4/index.js
@@ -0,0 +1,68 @@
+const e = require('express')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+let users = [
+  { id: 1, name: 'Usuário 1', email: 'usuario1@example.com'},
+  { id: 2, name: 'Usuário 2', email: 'usuario2@example.com'},
+  { id: 3, name: 'Usuário 3', email: 'usuario3@example.com'}
+]
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.get('/users/:id', (req, res) => {
+   const id = Number(req.params.id)
+   const user = users.find(u => u.id === id) 
+   if (!user) {
+     res.status(404).json({ message: 'Usuário não encontrado' })
+   } else {
+     res.json(user)
+   }
+})
+
+app.post('/users', (req, res) => {
+  const { name, email } = req.body
+  if (!name || !email) {
+    return res.status(400).json({ message: 'Nome e email são obrigatórios' })
+  }
+  const newUser = {
+    id: users.length + 1,
+    name,
+    email
+  }
+  users.push(newUser)
+  res.status(201).json(newUser)
+})
+
+app.put('/users/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const user = users.find(u => u.id === id)
+  if (!user) {
+    return res.status(404).json({ error: 'Usuário não encontrado' }) 
+  }
+    {
+    const { name, email } = req.body
+    if (name) user.name = name
+    if (email) user.email = email
+    res.json(user)
+  } 
+})
+
+app.delete('/users/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const userIndex = users.findIndex(u => u.id === id)
+  if (userIndex === -1) {
+    return res.status(404).json({ message: 'Usuário não encontrado' })
+  }
+  const deletedUser =users.splice(userIndex, 1)
+  res.json(deletedUser[0])
+})
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`)
+}) 
---
+++ Henrique-Rodrigues/Tarefa-4/readme.md
@@ -0,0 +1,17 @@
+Para testar os comandos seguir o passo a passo:
+
+GET: Acessar o URL http://localhost:3000/users ou http://localhost:3000/users/id
+
+POST: Abrir um novo terminal e executar o comando:
+curl.exe -X POST http://localhost:3000/users `
+  -H "Content-Type: application/json" `
+  -d '{"name":"Usuário4","email":"usuario4@email.com"}'
+
+PUT: Abrir um novo terminal e executar o comando:
+curl.exe -X PUT http://localhost:3000/users/2 `
+  -H "Content-Type: application/json" `
+  -d '{"name":"Nome Atualizado"}'
+
+DELETE: curl.exe -X DELETE http://localhost:3000/users/id
+
+OBS.: O projeto precisa estar rodando para que as mudanças sejam vistas.
